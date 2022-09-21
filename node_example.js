298161b87640141ded78ef2675e262c8a8538476
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-09-21 11:49:09 +0900

    server

---
+++ node_example.js
@@ -0,0 +1,11 @@
+import http from 'http';
+import fs from 'fs';
+
+http.createServer(function(request, response) {
+  const data = fs.readFileSync('./index.html');
+  response.writeHead(200,{"content-type": "text/html; charset=utf-8"});
+  response.write(data);
+  response.end();
+}).listen(0976,function(){
+  console.log('서버가 작동되고 있습니다!');
+});
